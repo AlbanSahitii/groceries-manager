4c485f80f2dc6ec05df466872ecfdf16f16f2222
AlbanSahitii <albansahiti2002@gmail.com>
2024-11-12 16:04:24 +0100

    jwt auth

---
+++ server/utils/auth/JwtService.js
@@ -0,0 +1,37 @@
+const jwt = require('jsonwebtoken')
+const secretKey = process.env.JWT_KEY
+
+class JwtService {
+
+    static generateJwt = (payload) => {
+        /*
+        payload needs to receive 
+        {
+        useername: username
+        password : password
+        }
+        */
+        try {
+            const token = jwt.sign(payload, secretKey);
+            return token;
+    
+        } catch (error) {
+            return error
+        }
+    }
+
+    static verifyJwt = (payload) => {
+
+        try {
+            const decoded = jwt.verify(token, secretKey);
+      
+            return decoded;
+            
+          } catch (err) {
+            console.error('JWT verification failed:', err.message);
+            return null;
+          }
+    }
+}
+
+module.exports = JwtService

c15b85b4f49b54dd00cdf6b69c47379ffc7e2598
AlbanSahitii <109045631+AlbanSahitii@users.noreply.github.com>
2025-02-14 11:16:54 +0100

    new main page with css

--- app/src/components/GuestPage/Main.js
+++ app/src/components/GuestPage/Main.js
@@ -1,30 +1,36 @@
 import { useNavigate } from 'react-router-dom';
-
 import carrotLogo from './src/img/carrot-icon.png'
+import backgroundImage from '../src/img/background-fruits.png'
+
+import './src/styles/main-page.css'
+
 
 const Main = () => {
     const navigate = useNavigate()
     return (
         <>
-        <nav>
-            <div className="logo">
-                <img alt='logo'></img>
+        <nav> 
+            <div className='nav-container'>
+                <div className="logo">
+                    <img alt='logo' src={carrotLogo}></img>
+                </div>
+                <div className='nav-information'>
+                    <a href='/about'>About</a>
+                    <a href='/features'>Features</a>
+                </div>
+                <div className="nav-auth">
+                    <a href='/login'>Login</a>
+                    <a href='/register'>Register</a>
+                </div>
             </div>
-            <div className='nav-information'>
-                <a href='/about'>About</a>
-                <a href='/features'>Features</a>
-            </div>
-            <div className="nav-auth">
-                <button onClick={()=> navigate("/login")}> Login</button>
-                <button onClick={()=> navigate("/register")}> Register</button>
 
-            </div>
         </nav>
 
         <section className='hero-section'>
             <h1>SyncBasket</h1>
             <p>Do groceries in smart way.</p>
             <button onClick={()=> navigate('/register')}>Get Started</button>
+            
         </section>
 
         </>
